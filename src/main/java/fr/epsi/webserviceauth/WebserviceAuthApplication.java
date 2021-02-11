d5239a57326ff3b98ea0456a1976cad85d935b95
Thibault Doucet <thibault.doucet44@gmail.com>
2021-02-11 16:36:44 +0100

    :tada: Initial commit

---
+++ src/main/java/fr/epsi/webserviceauth/WebserviceAuthApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.webserviceauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/webserviceauth/WebserviceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.webserviceauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
