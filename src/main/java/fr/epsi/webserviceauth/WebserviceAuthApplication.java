d703f79d5d87bc4f11d597ca70943cacddd6a5d1
Thibault Doucet <thibault.doucet44@gmail.com>
2021-02-15 10:44:47 +0100

    :wrench: Updated application main

--- src/main/java/fr/epsi/webserviceauth/WebserviceAuthApplication.java
+++ src/main/java/fr/epsi/webserviceauth/WebserviceAuthApplication.java
@@ -2,12 +2,13 @@ package fr.epsi.webserviceauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableAuthorizationServer
 @SpringBootApplication
-public class WebserviceAuthApplication {
+public class WebserviceAuthApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceAuthApplication.class, args);
